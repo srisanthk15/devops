1d0fcbf65ebad8e8e1ed158cac9d7aab784eb27d
srisanthk15 <srisanthk15@gmail.com>
2015-12-29 11:39:15 +0530

    added test repo

--- chef-repo/cookbooks/chefdk_getting_started/README.md
+++
@@ -1,4 +0,0 @@
-# chefdk_getting_started
-
-TODO: Enter the cookbook description here.
-
--- chef-repo/cookbooks/chefdk_getting_started/recipes/default.rb
+++
@@ -1,5 +0,0 @@
-#
-# Cookbook Name:: chefdk_getting_started
-# Recipe:: default
-#
-# Copyright (c) 2015 The Authors, All Rights Reserved.
